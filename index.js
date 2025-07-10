ca0d119ad2f8756f36cb28490c31601d58034e7d
AnaPaula123416 <paula.barco@estudante.ifro.edu.br>
2025-07-10 11:40:05 -0400

    10.072025

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const calculaIMC = require ('./servicos/calculadoraIMC');
 
 app.get('/',(req, res) => {
-    res.send('Hello World!');
+    let peso = req.query.peso;
+    let altura = req.query.altura;
+
+    if (calculaIMC.validaParametro(peso)&&calculaIMC.validaParametro(altura)) 
+    {
+    let imc = calculaIMC.efetuarCalculoIMC(peso,altura);
+    let status = calculaIMC.retornarStatusIMC(imc);
+
+    let json = {imc: imc, status: status};
+    res.json(json);}
+    
+    else{
+        res.status(400).json({ erro: 'Peso ou altura inválidos'})
+    }
+    
 });
 
 app.listen(8080, () => {
     let data = new Date();
     console.log("Servidor node iniciando em: " + data)
-})
+});
+
